1d51ad3d55bafbe63ce637fd73a574b407d7eb71
Danylo Romanchak <dan.romanch@gmail.com>
2024-03-03 18:12:52 +0200

    add code and test lab2

---
+++ test/test2_3_2.py
@@ -0,0 +1,20 @@
+import unittest
+from src.lab2_3_2 import put_on_board
+
+
+class TestPutOnBoard(unittest.TestCase):
+    def test_given_numbers(self):
+        result = put_on_board(10, 2, 3)
+        self.assertEqual(result, 9)
+
+    def test_big_numbers(self):
+        result = put_on_board(2, 1000000000, 999999999)
+        self.assertEqual(result, 1999999998)
+
+    def test_small_numbers(self):
+        result = put_on_board(4, 1, 1)
+        self.assertEqual(result, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
