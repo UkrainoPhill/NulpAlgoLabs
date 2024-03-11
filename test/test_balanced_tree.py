ef98c04e9e998bb6bc185fed3b56fe84cc7e03a6
Danylo Romanchak <dan.romanch@gmail.com>
2024-03-12 00:26:38 +0200

    add lab balanced_tree fixed import in test and expand Readme.md

---
+++ test/test_balanced_tree.py
@@ -0,0 +1,17 @@
+import unittest
+from NulpAlgoLabs.src.balanced_tree import *
+
+
+class TestBalancedTree(unittest.TestCase):
+    def test_given_tree(self):
+        root = BinaryTree(1)
+        root.left = BinaryTree(2)
+        root.right = BinaryTree(3)
+        root.left.left = BinaryTree(4)
+        root.left.right = BinaryTree(5)
+        result = balanced_tree(root)
+        self.assertEqual(result, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
