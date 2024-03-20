178c2a19efe778571ac93a917dcc87c2d82a6039
Danylo Romanchak <147646942+UkrainoPhill@users.noreply.github.com>
2024-03-21 00:15:11 +0200

    Add binary_tree_priority_queue and test (#4)
    * Add binary_tree_priority_queue and test
    
    * Add empty test

---
+++ test/test_binary_tree_priority_queue.py
@@ -0,0 +1,43 @@
+import unittest
+from NulpAlgoLabs.src.binary_tree_priority_queue import *
+
+
+class TestFindLongestPick(unittest.TestCase):
+    def test_delete_max_priority_function(self):
+        tree = BinaryTree()
+        tree.is_root_none_insert(1.5, 0.1)
+        tree.is_root_none_insert(2, 1)
+        tree.is_root_none_insert(3, 1)
+        tree.is_root_none_insert(2, 2.5)
+        tree.is_root_none_insert(3, 3)
+        tree.is_root_none_insert(4.3, 4)
+
+        self.assertEqual(tree.is_root_none_pop(), (4.3, 4))
+
+    def test_equal_priority_input_data(self):
+        tree = BinaryTree()
+        tree.is_root_none_insert(1, 1)
+        tree.is_root_none_insert(2, 1)
+        tree.is_root_none_insert(3, 1)
+        tree.is_root_none_insert(4, 1)
+        tree.is_root_none_insert(5, 1)
+
+        self.assertEqual(tree.root.left.value, 2)
+
+    def test_increasing_priority_input_data(self):
+        tree = BinaryTree()
+        tree.is_root_none_insert(1, 1)
+        tree.is_root_none_insert(11, 2)
+        tree.is_root_none_insert(111, 3)
+        tree.is_root_none_insert(1111, 4)
+        tree.is_root_none_insert(11111, 5)
+
+        self.assertEqual(tree.root.left.value, 11)
+
+    def test_empty_tree(self):
+        tree = BinaryTree()
+        self.assertEqual(tree.is_root_none_pop(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
